572cd6c110b147ab4290b7a7def2f1eb43eecd9b
TheZetClaudio <zeter54.klaudiusz@gmail.com>
2024-01-08 23:16:10 +0100

    Updata

--- OpeApp/Models/Koment.cs
+++ OpeApp/Models/Koment.cs
@@ -1,6 +1,14 @@
-﻿namespace OpeApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpeApp.Models
 {
     public class Koment
     {
+        [Key] public int KomID { get; set; }
+
+        [Required(ErrorMessage = "Podaj ID Wpisu do którego ma przynależeć komentarz!")]
+        [Column(TypeName = "nvarchar(50)")] public string WpisID { get; set; }
+        [Column(TypeName = "nvarchar(100)")] public string? KomOpis { get; set; }
     }
 }
