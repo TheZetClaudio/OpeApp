67aadbb32e296b22e40a5b0487027359d38a0ff0
TheZetClaudio <zeter54.klaudiusz@gmail.com>
2024-01-08 23:13:10 +0100

    Updata

--- OpeApp/Models/Wpis.cs
+++ OpeApp/Models/Wpis.cs
@@ -1,6 +1,14 @@
-﻿namespace OpeApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpeApp.Models
 {
     public class Wpis
     {
+        [Key] public int WpisID { get; set; }
+
+        [Required(ErrorMessage = "Podaj Nazwe")]
+        [Column(TypeName = "nvarchar(50)")] public string WpisName { get; set; }
+        [Column(TypeName = "nvarchar(500)")] public string? WpisOpis { get; set; }
     }
 }
